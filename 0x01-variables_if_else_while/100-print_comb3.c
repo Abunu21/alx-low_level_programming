1229198b100ed067c14b85b76992eda7cecf8e9f
Abunu21 <abunutesfaw@gmail.com>
2022-04-08 23:23:56 -0700

    100-print_comb3.c script

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,7 @@
 * Description:-Write a program that prints all possible
 * different combinations of two digits
 * Numbers must be separated by ,, followed by a space
-* Return : 0 
+* Return : 0 Success
 */
 int main(void)
 {
