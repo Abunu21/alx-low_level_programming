f74c4a2ba47d37a66d1bb55322c2557556287642
Abunu21 <abunutesfaw@gmail.com>
2022-04-12 19:06:39 +0300

    0-putchar.c script

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,20 @@
  *
  * Return: 0
  */
- int main(void)
+int main(void)
 {
-	char text[8] = "_putchar";
-	int i = 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+return (0);
 }
+
 
  
