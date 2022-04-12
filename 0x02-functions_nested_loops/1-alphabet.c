d0830911d600051946fe86e08b9a9e3420faa6b8
Abunu21 <abunutesfaw@gmail.com>
2022-04-12 19:12:51 +0300

    1-alphabet.c script

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,15 @@
 
 void print_alphabet(void)
 {
-char alphabet;
+	char c = 'a';
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
 }
 
-_putchar('\n');
-
-}
